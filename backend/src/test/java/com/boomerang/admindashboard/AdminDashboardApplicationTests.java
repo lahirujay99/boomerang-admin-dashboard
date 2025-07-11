9eb68e93b49ade52995db209a721298febd7a5c8
Lahiru Jayawardhane <93923625+lahirujay99@users.noreply.github.com>
2025-07-11 12:04:29 +0530

    feat: initial project setup and database configuration

---
+++ backend/src/test/java/com/boomerang/admindashboard/AdminDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boomerang.admindashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
